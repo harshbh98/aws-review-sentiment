065015b5e926e55028c6d4cf9197bb2ce2f84623
Harsh Bhardwaj <61739620+harshbh98@users.noreply.github.com>
2026-03-29 23:57:35 +0530

    restore the required version

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@
 # ============================================================
 
 terraform {
-  required_version = "~> 1.14.0"
+  required_version = ">= 1.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
